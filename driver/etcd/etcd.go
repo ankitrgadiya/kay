f44d46d59598188846a8add53b8c1e40f968c366
Ankit R Gadiya <git@argp.in>
2021-12-19 18:02:10 +0530

    etcd: adds driver for etcd
    Signed-off-by: Ankit R Gadiya <git@argp.in>

---
+++ driver/etcd/etcd.go
@@ -0,0 +1,86 @@
+package etcd
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	clientv3 "go.etcd.io/etcd/client/v3"
+
+	"argc.in/kay/kv"
+)
+
+func init() {
+	kv.Register("etcd", kv.DriverFunc(func() kv.KeyValue {
+		return new(impl)
+	}))
+}
+
+type impl struct {
+	Endpoints []string `ini:"endpoints"`
+	Username  string   `ini:"username,omitempty"`
+	Password  string   `ini:"password,omitempty"`
+
+	db *clientv3.Client
+}
+
+func (i *impl) Init() error {
+	db, err := clientv3.New(i.clientConfig())
+	if err != nil {
+		return err
+	}
+
+	i.db = db
+	return nil
+}
+
+func (i *impl) Close() error {
+	if err := i.db.Close(); err != nil {
+		return errors.Wrap(err, "closing")
+	}
+
+	return nil
+}
+
+func (i *impl) Get(key string) ([]byte, error) {
+	resp, err := i.db.Get(context.Background(), key)
+	if err != nil {
+		return nil, errors.Wrapf(err, "getting key: %s", key)
+	}
+
+	if len(resp.Kvs) == 0 {
+		return nil, errors.Errorf("key not found: %s", key)
+	}
+
+	kv := resp.Kvs[0]
+	if kv == nil {
+		return nil, errors.Errorf("invalid response for key: %s", key)
+	}
+
+	return kv.Value, nil
+}
+
+func (i *impl) Set(key string, value []byte) error {
+	_, err := i.db.Put(context.Background(), key, string(value))
+	if err != nil {
+		return errors.Wrapf(err, "setting key: %s", key)
+	}
+
+	return nil
+}
+
+func (i *impl) Delete(key string) error {
+	_, err := i.db.Delete(context.Background(), key)
+	if err != nil {
+		return errors.Wrapf(err, "deleting key: %s", key)
+	}
+
+	return nil
+}
+
+func (i *impl) clientConfig() clientv3.Config {
+	return clientv3.Config{
+		Endpoints: i.Endpoints,
+		Username:  i.Username,
+		Password:  i.Password,
+	}
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/spf13/cobra v0.0.5
 	github.com/syndtr/goleveldb v1.0.0
 	go.etcd.io/bbolt v1.3.6
+	go.etcd.io/etcd/client/v3 v3.5.1
 	gopkg.in/ini.v1 v1.66.2
 )
 
@@ -19,14 +20,26 @@ require (
 	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
 	github.com/cockroachdb/redact v1.0.8 // indirect
 	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kr/text v0.1.0 // indirect
 	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.1 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/exp v0.0.0-20200513190911-00229845015e // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210909193231-528a39cd75f3 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
